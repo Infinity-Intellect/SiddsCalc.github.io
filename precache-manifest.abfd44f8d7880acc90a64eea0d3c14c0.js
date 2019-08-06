e7b5ffe99e37ff484baa905d315837bac42e6c5c
Infinity-Intellect <snavaneetharan@gmail.com>
2019-08-06 22:48:42 +0530

    Updates

---
+++ precache-manifest.abfd44f8d7880acc90a64eea0d3c14c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bb7d6e2aa656fac5fb207c41950e68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6598df6d723efd83c139",
+    "url": "/static/css/main.9c311e99.chunk.css"
+  },
+  {
+    "revision": "cc64a1bae44a7c1f5e16",
+    "url": "/static/js/2.a9758499.chunk.js"
+  },
+  {
+    "revision": "6598df6d723efd83c139",
+    "url": "/static/js/main.3a7cf1d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/components/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Button = (props) =>{
-    return(
-        <input type="button" value={props.display}  onClick={props.handleClick}></input>
-    )
-}
-export default Button;
